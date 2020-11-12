f0f97a254f50551de508516990af63a1c4b31d8c
Anders Kok <ak.kornerup@gmail.com>
2020-11-12 22:29:51 +0100

    Updated about page

--- src/app/views/about/about.module.ts
+++ src/app/views/about/about.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './page/about.component';
+import { SharedComponentsModule } from "../../shared/components/shared-components.module";
 
 
 @NgModule({
   declarations: [AboutComponent],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    SharedComponentsModule
   ]
 })
 export class AboutModule { }
