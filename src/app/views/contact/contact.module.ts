4a77e6ac74f8e464292bf2c421570c66a75f0cc5
Anders Kok <ak.kornerup@gmail.com>
2020-11-16 22:30:21 +0100

    Added contact page

--- src/app/views/contact/contact.module.ts
+++ src/app/views/contact/contact.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './page/contact.component';
+import { SharedComponentsModule } from 'src/app/shared/components/shared-components.module';
+import { MatIconModule } from "@angular/material/icon";
 
 
 @NgModule({
   declarations: [ContactComponent],
   imports: [
     CommonModule,
-    ContactRoutingModule
+    ContactRoutingModule,
+    SharedComponentsModule,
+    MatIconModule
   ]
 })
 export class ContactModule { }
