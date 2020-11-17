4ba82d94a40a1901abebb6ec6caeaf1c84d83b12
Anders Kok <ak.kornerup@gmail.com>
2020-11-17 19:26:50 +0100

    Fixed video hero not autoplaying

--- src/app/views/home/page/home.component.ts
+++ src/app/views/home/page/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+interface HTMLEvent<T> extends Event {
+  target: EventTarget & T;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('video') videoElm: ElementRef<HTMLVideoElement>;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  ngAfterViewInit() {
+    // this.videoElm.nativeElement.muted = true;
+    // this.videoElm.nativeElement.play();
   }
 
+  playVideo(video: HTMLEvent<HTMLVideoElement>): void {
+    video.target.muted = true;
+    video.target.play();
+  }
 }
